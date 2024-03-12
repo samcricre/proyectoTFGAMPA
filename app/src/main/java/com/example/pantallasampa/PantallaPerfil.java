7809972dc988a2170199e662228778f49bba53a9
samcricre <samcricre@gmail.com>
2024-03-13 00:18:29 +0100

    Creación de página de perfil de usuario

---
+++ app/src/main/java/com/example/pantallasampa/PantallaPerfil.java
@@ -0,0 +1,14 @@
+package com.example.pantallasampa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PantallaPerfil extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pantalla_perfil);
+    }
+}
