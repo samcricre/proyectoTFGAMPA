0d94244ac76ec869f52b669245328f161efe2fab
samcricre <samcricre@gmail.com>
2024-03-12 22:38:02 +0100

    Creación de páginas de inicio sesion y registro

--- app/src/main/java/com/example/pantallasampa/PantallaCorreo.java
+++ app/src/main/java/com/example/pantallasampa/PantallaCorreo.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class correo extends AppCompatActivity {
+public class PantallaCorreo extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_correo);
+        setContentView(R.layout.activity_pantalla_correo);
     }
 }
