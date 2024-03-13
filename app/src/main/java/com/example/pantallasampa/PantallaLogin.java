448744cf888974627bf49d88c3db8d5c9d44c7f7
Zeyi <zeyilin222@gmail.com>
2024-03-13 15:06:14 +0100

    commit_Zeyi: Login_v0.9 (Terminado, falta Intent y testearlo) y Registro_v0.5(Falta los demás campos almacenarlo en la base de datos)

--- app/src/main/java/com/example/pantallasampa/PantallaLogin.java
+++ app/src/main/java/com/example/pantallasampa/PantallaLogin.java
@@ -26,6 +26,18 @@ public class PantallaLogin extends AppCompatActivity {
     private TextView irRegister;
     private FirebaseAuth mAuth;
 
+    /*
+     * Pantalla login
+     *
+     *          [Terminado (falta testing y cambiar el intent)]
+     *
+     * En esta pantalla simplemente tendremos que introducir el correo con el que nos hemos
+     * registrado. En el caso de que no tengamos correo, podremos darle al mensaje azul, el cual
+     * nos redirige a una nueva pantalla donde podremos crearnos una cuenta. En el caso de que si
+     * tengamos una cuenta, tendremos que introducir el email y la contraseñá, los cuales serán
+     * verificados gracias a signInWithEmailAndPassword() de FirebaseAuth
+     * */
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,36 +48,43 @@ public class PantallaLogin extends AppCompatActivity {
         passw = findViewById(R.id.passwBoxLogin);
         accessButton = findViewById(R.id.btAccederLogin);
         irRegister = findViewById(R.id.irResgistro);
+        mAuth = FirebaseAuth.getInstance();//Inicializamos la variable dando la isntancia
 
-        mAuth = FirebaseAuth.getInstance();
-
-
+        //Si le damos al botón de iniciar sessión
         accessButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                //variables necesarias que almacenan los datos de email y passw como Strings
                 String emailAux = email.getText().toString();
                 String passwAux = passw.getText().toString();
 
                 //Validamos que todos los campos estén rellenados
                 if(emailAux.isEmpty()||passwAux.isEmpty()){
+                    //Si no devuelve un mensaje y para la función con un return
                     Toast.makeText(PantallaLogin.this,
                             "Todos los campos son obligatorios", Toast.LENGTH_SHORT).show();
                     return;
                 }
+
+                //En el caso de que esté correcto, se realizará signInWithEmailAndPassword(), el
+                //cual verifica si el usuario existe por el email y el passw
                 mAuth.signInWithEmailAndPassword(emailAux,passwAux)
                         .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                             @Override
                             public void onComplete(@NonNull Task<AuthResult> task) {
                                 if(task.isSuccessful()){
+                                    //Si se consegue, se nos enviará a la siguiente pantalla
+                                    //  [El intent debe cambiarse para la versión final]
                                     Intent intent =
                                             new Intent(PantallaLogin.this,
                                                     PantallaInicial.class);
-                                    intent.putExtra("email",emailAux);
                                     startActivity(intent);
                                 }
                                 else{
+                                    //En el caso de fracaso, se nos envía una notifiación
                                     Toast.makeText(PantallaLogin.this,
-                                            "Error al iniciar sesión",
+                                            "Usuario o contraseña incorrectas",
                                             Toast.LENGTH_SHORT).show();
                                 }
                             }
@@ -73,7 +92,7 @@ public class PantallaLogin extends AppCompatActivity {
             }
         });
 
-        //Creamos un onClic para ir a la pantalla de registro
+        //Creamos un onClic para ir a la pantalla de registro con el uso de un Intent
         irRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -81,8 +100,6 @@ public class PantallaLogin extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
-
 
     }
 
--- app/src/main/java/com/example/pantallasampa/PantallaRegistro.java
+++ app/src/main/java/com/example/pantallasampa/PantallaRegistro.java
@@ -1,14 +1,92 @@
 package com.example.pantallasampa;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class PantallaRegistro extends AppCompatActivity {
 
+    //Variables necesarias
+    private Button register;
+    private EditText email;
+    private EditText passw;
+    private EditText passwConfirm;
+    private FirebaseAuth mAuth;
+
+    /*
+    * Pantalla registro
+    *
+    *       [Por ahora esto es una versión no terminada de la pantalla registro]
+    *
+    * En esta pantalla insertaremos los distintos datos con los que nos registraremos con la ayuda
+    * de FirebaseAuth. Con esta, podremos utilizara createUserWithEmailAndPassword(), función que
+    * nos permite crear un usuario instroduciendo la contraseña y el correo electrónico.
+    * */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantalla_registro);
+
+        //Declaramos las variables
+        register = findViewById(R.id.registerButton);
+        email = findViewById(R.id.emailRegister);
+        passw = findViewById(R.id.passwRegister);
+        passwConfirm = findViewById(R.id.passwConfiRegister);
+        mAuth = FirebaseAuth.getInstance();//Damos la isntancia a mAuth para inicializarla
+
+        //Cuando le demos al botón de registro
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //Variables auxiliares que almacenarán los campos requeridos
+                //      [De momento solo estará email y passw]
+                String emailAux = email.getText().toString();
+                String passwAux = passw.getText().toString();
+
+                //Confirmamos que no está vacíos los parametros obligatorios
+                if(emailAux.isEmpty()||passwAux.isEmpty()){
+                    //Si es el caso, se notifica y se para la función con un return
+                    Toast.makeText(PantallaRegistro.this,
+                            "Todos los campos son obligatorios", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                //En el caso de que la validación sea correcta creanos el usuario
+                mAuth.createUserWithEmailAndPassword(emailAux,passwAux)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if(task.isSuccessful()){
+                                    //En el caso de conseguirlo nos enviará a otra página
+                                    // [En esta caso nos envía al inicio]
+                                    Intent intent =
+                                            new Intent(PantallaRegistro.this,
+                                                    PantallaInicial.class);
+                                    startActivity(intent);
+                                }
+                                else{
+                                    //En el caso de fracaso, notificaremos que ya existe la cuenta
+                                    Toast.makeText(PantallaRegistro.this,
+                                            "Error, la cuenta ya existe",
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+            }
+        });
+
     }
 }
