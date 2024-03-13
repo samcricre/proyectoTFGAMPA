8cd11b744929779d2669972f90e6484966003ab1
Zeyi <zeyilin222@gmail.com>
2024-03-13 14:13:25 +0100

    Primer commit_Zeyi: Login_v0.5 (Terminado, pero falta testing)

--- app/src/main/java/com/example/pantallasampa/PantallaLogin.java
+++ app/src/main/java/com/example/pantallasampa/PantallaLogin.java
@@ -1,11 +1,21 @@
 package com.example.pantallasampa;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
 
 public class PantallaLogin extends AppCompatActivity {
 
@@ -14,6 +24,7 @@ public class PantallaLogin extends AppCompatActivity {
     private EditText passw;
     private Button accessButton;
     private TextView irRegister;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,10 +37,53 @@ public class PantallaLogin extends AppCompatActivity {
         accessButton = findViewById(R.id.btAccederLogin);
         irRegister = findViewById(R.id.irResgistro);
 
+        mAuth = FirebaseAuth.getInstance();
 
 
+        accessButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String emailAux = email.getText().toString();
+                String passwAux = passw.getText().toString();
+
+                //Validamos que todos los campos estén rellenados
+                if(emailAux.isEmpty()||passwAux.isEmpty()){
+                    Toast.makeText(PantallaLogin.this,
+                            "Todos los campos son obligatorios", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                mAuth.signInWithEmailAndPassword(emailAux,passwAux)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if(task.isSuccessful()){
+                                    Intent intent =
+                                            new Intent(PantallaLogin.this,
+                                                    PantallaInicial.class);
+                                    intent.putExtra("email",emailAux);
+                                    startActivity(intent);
+                                }
+                                else{
+                                    Toast.makeText(PantallaLogin.this,
+                                            "Error al iniciar sesión",
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+            }
+        });
+
+        //Creamos un onClic para ir a la pantalla de registro
+        irRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(PantallaLogin.this, PantallaRegistro.class);
+                startActivity(intent);
+            }
+        });
 
 
 
     }
+
 }
