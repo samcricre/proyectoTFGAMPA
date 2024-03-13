b48c0e62b7885e301fa70450c146f206a14c8a57
Zeyi <zeyilin222@gmail.com>
2024-03-13 13:23:26 +0100

    Primer commit_Zeyi: Login_v0.1

--- app/src/main/java/com/example/pantallasampa/PantallaLogin.java
+++ app/src/main/java/com/example/pantallasampa/PantallaLogin.java
@@ -3,12 +3,33 @@ package com.example.pantallasampa;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class PantallaLogin extends AppCompatActivity {
+
+    //Variables necesarias
+    private EditText email;
+    private EditText passw;
+    private Button accessButton;
+    private TextView irRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantalla_login);
+
+        //Inivializaremos nuestras variables
+        email = findViewById(R.id.emailBoxLogin);
+        passw = findViewById(R.id.passwBoxLogin);
+        accessButton = findViewById(R.id.btAccederLogin);
+        irRegister = findViewById(R.id.irResgistro);
+
+
+
+
+
+
     }
 }
