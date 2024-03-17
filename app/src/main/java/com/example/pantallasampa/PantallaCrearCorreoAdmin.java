ed926e37cea4356541d8c62fc7d094ee9ed88b1f
Tamuzur <jerjesiv@gmail.com>
2024-03-17 19:50:08 +0100

    paginas sorteo y crear sorteo incompletas, falta cambiar el adaptador y luego mostrar los sorteos

--- app/src/main/java/com/example/pantallasampa/PantallaCrearCorreoAdmin.java
+++ app/src/main/java/com/example/pantallasampa/PantallaCrearCorreoAdmin.java
@@ -3,12 +3,12 @@ package com.example.pantallasampa;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
-import android.widget.AdapterView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -20,6 +20,10 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.ValueEventListener;
 
 public class PantallaCrearCorreoAdmin extends AppCompatActivity {
 
@@ -98,19 +102,141 @@ public class PantallaCrearCorreoAdmin extends AppCompatActivity {
         // Validar el tipo de correo y tomar acciones correspondientes
         switch (tipoCorreo) {
             case "Todos los usuarios no admins":
-                // Lógica para enviar correo a todos los usuarios no admins
-                // Implementa la lógica según tus requisitos
+                enviarCorreoATodosLosUsuariosNoAdmins(remitente, asunto, contenido);
                 break;
             case "Usuarios con hijos en una clase":
-                // Lógica para enviar correo a usuarios con hijos en una clase específica
-                // Implementa la lógica según tus requisitos
+                enviarCorreoAUsuariosConHijosEnClaseEspecifica(remitente, asunto, contenido,curso);
+
                 break;
             case "Persona específica":
-                // Lógica para enviar correo a una persona específica
-                // Implementa la lógica según tus requisitos
+                enviarCorreoAPersonaEspecifica(remitente, asunto, contenido);
                 break;
             default:
                 Toast.makeText(this, "Seleccione un tipo de correo", Toast.LENGTH_SHORT).show();
         }
     }
+
+    // Lógica para enviar correo a todos los usuarios no admins
+    private void enviarCorreoATodosLosUsuariosNoAdmins(String remitente, String asunto, String contenido) {
+        // Obtener una referencia a la base de datos de usuarios
+        DatabaseReference usersRef = FirebaseDatabase.getInstance().getReference().child("usuarios");
+
+        // Realizar una consulta para obtener todos los usuarios no admins
+        Query query = usersRef.orderByChild("roll").equalTo("user");
+
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                // Iterar sobre los usuarios y enviar correo a cada uno
+                for (DataSnapshot userSnapshot : dataSnapshot.getChildren()) {
+                    String destinatario = userSnapshot.child("email").getValue(String.class);
+                    enviarCorreoIndividual(remitente, destinatario, asunto, contenido);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                // Manejar el error en caso de que la consulta falle
+                Toast.makeText(PantallaCrearCorreoAdmin.this, "Error al obtener usuarios", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    // Método para enviar correo a un destinatario específico
+    private void enviarCorreoIndividual(String remitente, String destinatario, String asunto, String contenido) {
+        // Crear un objeto Correo con los datos ingresados
+        // Establecer los atributos eliminado y eliminadoRemitente como false por defecto
+        Correo correo = new Correo(null, remitente, destinatario, asunto, contenido, System.currentTimeMillis(), false, false);
+
+        // Guardar el correo en la base de datos
+        String correoId = correosRef.push().getKey(); // Obtener el ID único del correo
+        correo.setCorreoId(correoId); // Asignar el correoId al objeto Correo
+        correosRef.child(correoId).setValue(correo)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Toast.makeText(PantallaCrearCorreoAdmin.this, "Correo enviado correctamente", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(PantallaCrearCorreoAdmin.this, "Error al enviar el correo", Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    private void enviarCorreoAUsuariosConHijosEnClaseEspecifica(String remitente, String asunto, String contenido, String clase) {
+        // Obtener una referencia a la base de datos de usuarios
+        DatabaseReference usersRef = FirebaseDatabase.getInstance().getReference().child("usuarios");
+
+        // Realizar una consulta para obtener todos los usuarios que tienen al menos un hijo en la clase especificada
+        usersRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                // Iterar sobre los usuarios
+                for (DataSnapshot userSnapshot : dataSnapshot.getChildren()) {
+                    // Verificar si el usuario tiene hijos
+                    if (userSnapshot.hasChild("hijos")) {
+                        // Iterar sobre los hijos del usuario
+                        for (DataSnapshot hijoSnapshot : userSnapshot.child("hijos").getChildren()) {
+                            String claseHijo = hijoSnapshot.child("curso").getValue(String.class);
+                            // Verificar si el hijo asiste a la clase especificada
+                            if (claseHijo.equals(clase)) {
+                                // Enviar correo al usuario
+                                String destinatario = userSnapshot.child("email").getValue(String.class);
+                                enviarCorreoIndividual(remitente, destinatario, asunto, contenido);
+                                // Salir del bucle interno, ya que solo necesitamos enviar un correo por usuario
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                // Manejar el error en caso de que la consulta falle
+                Toast.makeText(PantallaCrearCorreoAdmin.this, "Error al obtener usuarios", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+    private void enviarCorreoAPersonaEspecifica(String asunto, String contenido, String destinatario) {
+        // Obtener el correo electrónico del remitente
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        if (currentUser == null) {
+            Toast.makeText(this, "Error: usuario no autenticado", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        String remitente = currentUser.getEmail();
+
+        // Crear un objeto Correo con los datos ingresados
+        // Establecer los atributos eliminado y eliminadoRemitente como false por defecto
+        Correo correo = new Correo(null, remitente, destinatario, asunto, contenido, System.currentTimeMillis(), false, false);
+
+        // Guardar el correo en la base de datos
+        String correoId = correosRef.push().getKey(); // Obtener el ID único del correo
+        correo.setCorreoId(correoId); // Asignar el correoId al objeto Correo
+        correosRef.child(correoId).setValue(correo)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Toast.makeText(PantallaCrearCorreoAdmin.this, "Correo enviado correctamente", Toast.LENGTH_SHORT).show();
+                        // Limpiar los campos de texto después de enviar el correo
+                        destinatarioEditText.setText("");
+                        asuntoEditText.setText("");
+                        contenidoEditText.setText("");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(PantallaCrearCorreoAdmin.this, "Error al enviar el correo", Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+
+
+
 }
---
+++ app/src/main/java/com/example/pantallasampa/PantallaCrearSorteo.java
@@ -0,0 +1,125 @@
+package com.example.pantallasampa;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class PantallaCrearSorteo extends AppCompatActivity {
+
+    private EditText nombreSorteoEditText, descripcionSorteoEditText, premiosSorteoEditText;
+    private Button btCrearSorteo;
+
+    private DatabaseReference sorteosRef, noticiasRef;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pantalla_crear_sorteo);
+
+        // Inicializar Firebase Database
+        sorteosRef = FirebaseDatabase.getInstance().getReference().child("sorteos");
+        noticiasRef = FirebaseDatabase.getInstance().getReference().child("noticias");
+
+        // Inicializar vistas
+        nombreSorteoEditText = findViewById(R.id.nombreSorteo);
+        descripcionSorteoEditText = findViewById(R.id.descripcionSorteo);
+        premiosSorteoEditText = findViewById(R.id.premiosSorteo);
+        btCrearSorteo = findViewById(R.id.btCrearSorteo);
+
+        // Configurar el botón para crear un sorteo
+        btCrearSorteo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                crearSorteoYNoticia();
+            }
+        });
+    }
+
+    private void crearSorteoYNoticia() {
+        // Obtener los valores de los campos
+        String nombreSorteo = nombreSorteoEditText.getText().toString().trim();
+        String descripcionSorteo = descripcionSorteoEditText.getText().toString().trim();
+        String premiosSorteo = premiosSorteoEditText.getText().toString().trim();
+
+        // Verificar si los campos están vacíos
+        if (nombreSorteo.isEmpty() || descripcionSorteo.isEmpty() || premiosSorteo.isEmpty()) {
+            Toast.makeText(this, "Por favor, complete todos los campos", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        // Obtener el usuario actualmente autenticado
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) {
+            String creador = user.getEmail();
+
+            // Crear un objeto Sorteo con los datos ingresados
+            Sorteos sorteo = new Sorteos(nombreSorteo, descripcionSorteo, premiosSorteo, creador, false, "");
+
+            // Guardar el sorteo en la base de datos de Firebase
+            sorteosRef.push().setValue(sorteo)
+                    .addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void aVoid) {
+                            // Crear una noticia con los datos del sorteo
+                            String mensajeNoticia = "Nuevo sorteo disponible: " + nombreSorteo;
+                            Noticia noticia = new Noticia(nombreSorteo, descripcionSorteo, mensajeNoticia, 0);
+
+                            // Guardar la noticia en la base de datos de Firebase
+                            noticiasRef.push().setValue(noticia)
+                                    .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                        @Override
+                                        public void onSuccess(Void aVoid) {
+                                            // Obtener la clave de la noticia recién creada
+                                            String noticiaKey = noticiasRef.getKey();
+                                            // Asignar la clave de la noticia al atributo correspondiente del sorteo
+                                            sorteosRef.child(noticiaKey).child("codigoNoticia").setValue(noticiaKey)
+                                                    .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                                        @Override
+                                                        public void onSuccess(Void aVoid) {
+                                                            Toast.makeText(PantallaCrearSorteo.this, "Sorteo y noticia creados correctamente", Toast.LENGTH_SHORT).show();
+                                                            // Limpiar los campos después de crear el sorteo
+                                                            nombreSorteoEditText.setText("");
+                                                            descripcionSorteoEditText.setText("");
+                                                            premiosSorteoEditText.setText("");
+                                                        }
+                                                    })
+                                                    .addOnFailureListener(new OnFailureListener() {
+                                                        @Override
+                                                        public void onFailure(@NonNull Exception e) {
+                                                            Toast.makeText(PantallaCrearSorteo.this, "Error al asignar la clave de la noticia al sorteo", Toast.LENGTH_SHORT).show();
+                                                        }
+                                                    });
+                                        }
+                                    })
+                                    .addOnFailureListener(new OnFailureListener() {
+                                        @Override
+                                        public void onFailure(@NonNull Exception e) {
+                                            Toast.makeText(PantallaCrearSorteo.this, "Error al crear la noticia", Toast.LENGTH_SHORT).show();
+                                        }
+                                    });
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(PantallaCrearSorteo.this, "Error al crear el sorteo", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+        } else {
+            Toast.makeText(this, "Usuario no autenticado", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
